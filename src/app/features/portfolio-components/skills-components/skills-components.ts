46ecae7d72579ee485400a76123b9cd1cae52f83
Maik G <maik.g@outlook.com>
2026-02-10 12:02:10 +0100

    Add: First step design skills component | add skill icons

--- src/app/features/portfolio-components/skills-components/skills-components.ts
+++ src/app/features/portfolio-components/skills-components/skills-components.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './skills-components.scss',
 })
 export class SkillsComponents {
+  skillsArry = [
+    'assets/icon/skills/Property 1=Angular.svg',
+    'assets/icon/skills/Property 1=Typescript.svg',
+    'assets/icon/skills/Property 1=JavScript.svg',
+    'assets/icon/skills/Property 1=html.svg',
+    'assets/icon/skills/Property 1=css.svg',
+    'assets/icon/skills/Property 1=Firebase.svg',
+    'assets/icon/skills/Property 1=Git.svg',
+    'assets/icon/skills/Property 1=Scrum.svg',
+    'assets/icon/skills/Property 1=Api.svg',
+    'assets/icon/skills/Property 1=Md.svg',
+  ]
 
 }
