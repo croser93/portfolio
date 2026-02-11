5da0180dd4e441d2937dacb5a9412e7b1895362a
Maik G <maik.g@outlook.com>
2026-02-11 17:36:07 +0100

    Revert "Edit: Img compressed"
    This reverts commit c6f85151cfc2a890e90b857ef0e466ca7fa24c2e.

--- src/app/features/portfolio-components/projects-component/projects-component.ts
+++ src/app/features/portfolio-components/projects-component/projects-component.ts
@@ -34,7 +34,7 @@ export class ProjectsComponent {
       title: 'Hellgate Shadowfall',
       codeBase: 'HTML | CSS | Javascript',
       description: 'A simple Jump-and-Run game based on object-oriented approach. Help Elvoron to find coins and mana to fight against the Hellknight.',
-      img: 'assets/img/projects/HellgateShadowfall.jpg',
+      img: 'assets/img/projects/HellgateShadowfall.JPG',
       github: 'https://github.com/croser93/2d-Game.git'
     },
      {
@@ -43,7 +43,7 @@ export class ProjectsComponent {
       title: 'Pokédex',
       codeBase: 'HTML | CSS | Javascript | Api',
       description: 'Based on the PokéAPI a simple libary hat provides and catalogues pokemon information-.',
-      img: 'assets/img/projects/pokedex.jpg',
+      img: 'assets/img/projects/pokedex.JPG',
       github: 'https://github.com/croser93/Pokedex.git'
     }
   ]
