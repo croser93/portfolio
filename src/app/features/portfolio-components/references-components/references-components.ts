3a7001351c6afd38b127def9d3c53dd716a1272c
Maik G <maik.g@outlook.com>
2026-02-11 18:17:05 +0100

    Add: 3 Comments and styling the content

--- src/app/features/portfolio-components/references-components/references-components.ts
+++ src/app/features/portfolio-components/references-components/references-components.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface comment {
+      id: number
+      name:string 
+      job:string 
+      description:string 
+}
+
 @Component({
   selector: 'app-references-components',
   imports: [],
@@ -7,5 +14,24 @@ import { Component } from '@angular/core';
   styleUrl: './references-components.scss',
 })
 export class ReferencesComponents {
-
+  humanComments: comment[] = [
+    {
+      id: 1,
+      name: 'V. Schuster',
+      job: 'Team Partner',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+    },
+        {
+      id: 1,
+      name: 'V. Schuster',
+      job: 'Team Partner',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+    },
+        {
+      id: 1,
+      name: 'V. Schuster',
+      job: 'Team Partner',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+    },
+  ]
 }
