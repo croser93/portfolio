1d53b77877121832742b056e2d4a57b69e808717
Maik G <maik.g@outlook.com>
2026-02-11 17:50:04 +0100

    Add: references component

--- src/app/features/portfolio-components/portfolio-components.ts
+++ src/app/features/portfolio-components/portfolio-components.ts
@@ -5,11 +5,12 @@ import { ContactComponents } from './contact-components/contact-components';
 import { SkillsComponents } from './skills-components/skills-components';
 import { Header } from '../shared/components/header/header';
 import { ProjectsComponent } from './projects-component/projects-component';
+import { ReferencesComponents } from './references-components/references-components';
 
 
 @Component({
   selector: 'app-portfolio-components',
-  imports: [HeroComponents, AboutMeComponents, ContactComponents, SkillsComponents, ProjectsComponent ,Header],
+  imports: [HeroComponents, AboutMeComponents, ContactComponents, SkillsComponents, ProjectsComponent ,ReferencesComponents ,Header],
   templateUrl: './portfolio-components.html',
   styleUrl: './portfolio-components.scss',
 })
---
+++ src/app/features/portfolio-components/references-components/references-components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-references-components',
+  imports: [],
+  templateUrl: './references-components.html',
+  styleUrl: './references-components.scss',
+})
+export class ReferencesComponents {
+
+}
