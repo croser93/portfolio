c5d9e339639c72e9ad787a9e80f848a5a3d91a1c
Maik G <maik.g@outlook.com>
2026-02-11 18:44:49 +0100

    Edit: Style comments | other description (dummy data)

--- src/app/features/portfolio-components/references-components/references-components.ts
+++ src/app/features/portfolio-components/references-components/references-components.ts
@@ -22,16 +22,16 @@ export class ReferencesComponents {
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
     },
         {
-      id: 1,
+      id: 2,
       name: 'V. Schuster',
       job: 'Team Partner',
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      description: 'Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals..',
     },
         {
-      id: 1,
+      id: 3,
       name: 'V. Schuster',
       job: 'Team Partner',
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      description: 'It was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.',
     },
   ]
 }
