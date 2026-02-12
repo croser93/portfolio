3eee2ef2db991512cc5a7f83a234e1083c05f549
Maik G <maik.g@outlook.com>
2026-02-12 14:08:27 +0100

    Add: sendMail.php and connect with app.config

--- src/app/features/portfolio-components/contact-components/contact-components.ts
+++ src/app/features/portfolio-components/contact-components/contact-components.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-components',
@@ -9,6 +10,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class ContactComponents {
  
+http = inject(HttpClient)
+
   contactData = {
     name: "",
     email: "",
@@ -16,8 +19,36 @@ export class ContactComponents {
 
   }
 
-  onSubmit(){
-    console.log(this.contactData)
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
 
 }
