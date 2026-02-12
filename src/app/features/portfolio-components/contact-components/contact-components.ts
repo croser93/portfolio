621af85e3f462fd2c5ab8459ba388a42b5d721d5
Maik G <maik.g@outlook.com>
2026-02-12 13:22:14 +0100

    feat: FormsModule and connect ts with html

--- src/app/features/portfolio-components/contact-components/contact-components.ts
+++ src/app/features/portfolio-components/contact-components/contact-components.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-components',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-components.html',
   styleUrl: './contact-components.scss',
 })
 export class ContactComponents {
+ 
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+
+  }
+
+  onSubmit(){
+    console.log(this.contactData)
+  }
 
 }
