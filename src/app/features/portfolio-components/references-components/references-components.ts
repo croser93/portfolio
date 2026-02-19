7936e94c59faa271d7d028c76bd642b7448c9521
Maik G <maik.g@outlook.com>
2026-02-19 17:10:21 +0100

    feat: Add a new interactive references carousel component with navigation and styling.

--- src/app/features/portfolio-components/references-components/references-components.ts
+++ src/app/features/portfolio-components/references-components/references-components.ts
@@ -18,15 +18,15 @@ export class ReferencesComponents {
   humanComments: comment[] = [
     {
       id: 1,
-      name: 'Lucas Graf',
-      job: 'Team Partner',
-      description: 'REFERENCES.1',
-    },
-        {
-      id: 2,
       name: 'Leon G Leuning',
       job: 'Team Partner',
       description: 'REFERENCES.2',
+    },
+        {
+      id: 2,
+      name: 'Lucas Graf',
+      job: 'Team Partner',
+      description: 'REFERENCES.1',
     },
         {
       id: 3,
@@ -35,4 +35,17 @@ export class ReferencesComponents {
       description: 'REFERENCES.3',
     },
   ]
+  
+  currentIndex = 1
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.humanComments.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.humanComments.length) % this.humanComments.length;
+  }
+
+  setSlide(index: number) {
+    this.currentIndex = index;
+  }
 }
