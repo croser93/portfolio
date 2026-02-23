7bf2ac6cbb627c5373494f2b5a069bb7067be34a
Maik G <maik.g@outlook.com>
2026-02-23 14:09:54 +0100

    Feat: language svae in Local storage | styling border in contact

--- src/app/features/shared/components/header/header.ts
+++ src/app/features/shared/components/header/header.ts
@@ -12,9 +12,21 @@ export class Header {
   isMenuOpen = false;
 
   private translate = inject(TranslateService);
+
+  constructor() {
+    const savedLang = localStorage.getItem('language') as 'en' | 'de';
+    if (savedLang) {
+      this.activeLanguage = savedLang;
+      this.translate.use(savedLang);
+    } else {
+      this.translate.use(this.activeLanguage);
+    }
+  }
+
   toggleLanguage(lang: 'en' | 'de') {
     this.activeLanguage = lang;
     this.translate.use(lang);
+    localStorage.setItem('language', lang);
   }
 
   toggleMenuBar() {
